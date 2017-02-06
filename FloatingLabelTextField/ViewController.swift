88257bedebfb3149a0a18ef4d898cb5b0f144d46
Daniel Langh <intonarumori@gmail.com>
2017-02-06 23:59:08 +0100

    Initial Commit

---
+++ FloatingLabelTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FloatingLabelTextField
+//
+//  Created by Daniel Langh on 2017. 02. 06..
+//  Copyright © 2017. rumori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
