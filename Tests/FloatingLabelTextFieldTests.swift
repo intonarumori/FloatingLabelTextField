52e96ac1446d0f147d6445e4e1aedfa66d18098a
Daniel Langh <intonarumori@gmail.com>
2017-02-07 23:09:49 +0100

    added tests

---
+++ Tests/FloatingLabelTextFieldTests.swift
@@ -0,0 +1,35 @@
+//
+//  FloatingLabelTextFieldTests.swift
+//  FloatingLabelTextFieldTests
+//
+//  Created by Daniel Langh on 2017. 02. 07..
+//  Copyright © 2017. rumori. All rights reserved.
+//
+
+import XCTest
+
+class FloatingLabelTextFieldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
