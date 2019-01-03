b6693e787fc519da57e2f90d3ee030a160a6f5d9
Daniel Langh <intonarumori@gmail.com>
2019-01-03 14:48:16 +0100

    added host application for tests

---
+++ FloatingLabelTextFieldTestHost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FloatingLabelTextFieldTestHost
+//
+//  Created by Daniel Langh on 2019. 01. 03..
+//  Copyright © 2019. rumori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
